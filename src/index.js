9cad6fe8f435e715e2d3c7da0adcfcb58b5a2b51
Juan Lulkin <maiz@lulk.in>
2017-03-04 02:57:50 +0200

    Improve nodes

--- src/index.js
+++ src/index.js
@@ -7,40 +7,100 @@ import filter from 'flyd/module/filter'
 import DOM from 'react-dom'
 import R from 'ramda'
 import css from 'index.css'
-import d3 from 'd3'
+import * as d3 from 'd3'
+
+const Header = {
+  title: 'Header',
+  ins: ['count'],
+  outs: {
+    dom: (count) => <h1>Count: {count}</h1>
+  }
+}
+
+const AddOne = () => {
+  const add = stream()
+
+  return {
+    title: 'AddOne',
+    ins: [],
+    outs: {
+      dom: () => <button onClick={() => add}>+</button>,
+      add
+    }
+  }
+}
+
+const DecOne = () => {
+  const dec = stream()
+
+  return {
+    title: 'AddOne',
+    ins: [],
+    outs: {
+      dom: () => <button onClick={() => dec}>-</button>,
+      dec
+    }
+  }
+}
 
 const initialState = {
   x: 0,
   y: 0,
   z: 1,
   width: window.innerWidth,
-  height: window.innerHeight
+  height: window.innerHeight,
+  graph: [Header, AddOne(), DecOne()]
 }
 
-const Arc = ({innerRadius, outerRadius, startAngle, endAngle}) => {
-  const arc = d3.svg.arc()
-    .innerRadius(innerRadius)
-    .outerRadius(outerRadius)
-    .startAngle(startAngle)
-    .endAngle(endAngle)
+// const arc =
+const Arc = ({r, a}) => {
+  const outerRadius = r
+  const innerRadius = r - 10
+  const [startAngle, endAngle] = a
+  const cornerRadius = 100000
+  const padAngle = 0.1
+  const props = {
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    // cornerRadius,
+    padAngle
+  }
 
+  return <path className={css.arc} d={d3.arc()(props)} />
+}
+
+const segments = (s) => (index, len) => {
+  const segmentSize = Math.PI / len
+  const start = segmentSize * index + s
+  const end = start + segmentSize
+  return [start, end]
+}
+const outSegments = segments(0)
+const inSegments = segments(Math.PI)
+
+const Node = ({ins, outs, x, y}) => {
   return (
-    <path
-      d={arc()}
-      fill={'yellow'}
-      stroke={'black'} />
+    <g transform={`translate(${x},${y})`}>
+      <g transform="translate(-2)">
+        {R.times((i) => <Arc key={`in-${i}`} r={100} a={inSegments(i, ins)} />, ins)}
+      </g>
+      <g transform="translate(2)">
+        {R.times((i) => <Arc key={`out-${i}`} r={100} a={outSegments(i, outs)} />, outs)}
+      </g>
+    </g>
   )
 }
 
-const graph = ({x, y, z, width, height}) => {
+const Graph = ({x, y, z, width, height, graph}) => {
   return (
     <svg width={width} height={height}>
       <g transform={`matrix(${z}, 0, 0, ${z}, ${x}, ${y})`}>
-        <circle r={100} cy={100} cx={100} fill='red' />
-        <text color='black' fill='black' fontSize={33} x={200} y={200}>!!!!!!</text>
-        <Arc innerRadius={50} outerRadius={100} startAngle={0} endAngle={90} />
+        {graph.map((item, i) => {
+            return <Node ins={item.ins.length} outs={Object.keys(item.outs).length} x={150 + i * 210} y={200} />
+        })}
       </g>
-
     </svg>
   )
 }
@@ -71,6 +131,7 @@ const move$ = R.pipe(
 const zoom$ = R.pipe(
   filter(R.prop('ctrlKey')),
   map(R.props(['deltaY'])),
+  map(R.head),
   map(action('zoom'))
 )(wheel)
 
@@ -91,13 +152,12 @@ const resize$ = R.pipe(
   map(action('resize'))
 )(resize)
 
-
 const reducer = (state, {type, payload}) => {
   switch (type) {
     case 'move':
       return {...state, x: state.x - payload[0], y: state.y - payload[1]}
     case 'zoom':
-      return {...state, z: state.z - payload[0]}
+      return {...state, z: Math.max(state.z - payload/100, 0.1)}
     case 'resize':
       return {...state, width: payload[0], height: payload[1]}
   }
@@ -108,7 +168,7 @@ const reducer = (state, {type, payload}) => {
 const state$ = scan(reducer, initialState, mergeAll([move$, zoom$, resize$]))
 
 const render = (...state) =>
-  DOM.render(graph(...state), document.getElementById('root'))
+  DOM.render(Graph(...state), document.getElementById('root'))
 
 map(render, state$)
-// map(console.log, state$)
+map(console.log, state$)
